455c9593e4f4e85187c376a17f856f3341df1955
Aroua91 <aroua.souabni@gmail.com>
2020-11-15 11:48:48 +0100

    Split project on maven modules

---
+++ workflow-data/src/main/java/com/ncq/devstudio/workflows/repositories/WorkflowRepository.java
@@ -0,0 +1,19 @@
+package com.ncq.devstudio.workflows.repositories;
+
+import com.ncq.devstudio.workflows.entities.Workflow;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author Aroua Souabni
+ */
+@Repository
+public interface WorkflowRepository extends PagingAndSortingRepository<Workflow, Integer>,
+        JpaSpecificationExecutor<Workflow> {
+
+    public Workflow findByUuid(String uuid);
+}
